f8c3f0af9087bf6d09c743d6aa8214b3a9fb285d
francesco gorini <francesco.gorini2000@yahoo.com>
2020-12-27 18:34:09 +1300

    Init commit| Create-react-app

---
+++ src/Components/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.css"
+
+function Nav() {
+    return (
+        <nav className="nav_container">
+            <ul className="nav_list">
+                <li><a href="">Home</a></li>
+                <li><a href="">Blog</a></li>
+                <li><a href="">About Me</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav;
