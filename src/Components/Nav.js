93ea77cff06d3da6c7141d16911e8f88e9ed48ea
francesco gorini <francesco.gorini2000@yahoo.com>
2021-01-09 15:35:04 +1300

    Theme color adjustments

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,14 +6,15 @@ function Nav() {
         <nav className="nav_container">
             <div>
                 <a url="https://www.digitalocean.com" href="/">
-                    <img src={logo} alt="Logo" className="logo" />
+                    <h1 className="logo">FG Blog</h1>
+                    {/* <img src={logo} alt="Logo" className="logo" /> */}
                 </a>
             </div>
             <ul className="nav_list">
-                <li><a href="">Home</a></li>
-                <li><a href="">Blog</a></li>
-                <li><a href="">About Me</a></li>
-                <li><a href="" className="highlight_btn">Sign Up</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/blog">Blog</a></li>
+                <li><a href="/about-me">About Me</a></li>
+                <li><a href="/signup" className="highlight_btn">Sign Up</a></li>
             </ul>
         </nav>
     )
