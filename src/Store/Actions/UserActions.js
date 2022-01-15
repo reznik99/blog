21ba2f1edd3b745942de5dfa221199124009296d
Francesco <goras.francesco@gmail.com>
2022-01-15 17:15:43 +1300

    Action and reducers to load post information from blog-api

--- src/Store/Actions/UserActions.js
+++ src/Store/Actions/UserActions.js
@@ -1,17 +1,37 @@
-import SelectInput from '@mui/material/Select/SelectInput'
 import { API_URL } from '../../Vars/Globals'
 
-export function loadPost() {
+export function loadPosts() {
     return async (dispatch, getState) => {
         try {
             dispatch({ type: "SET_LOADING", payload: true })
-            // fake delay for testing
-            const sleep = (milliseconds) => {
-                return new Promise(resolve => setTimeout(resolve, milliseconds))
-            }
-            await sleep(1000)
+            const response = await fetch("http://localhost:6969/posts")
+            const { posts } = await response.json()
+            dispatch({ type: "FETCH_POSTS_SUCCESS", payload: posts })
         } catch (err) {
-            console.log(`Couldn't find post in store: ${err}`)
+            console.log(`Unable to load posts ${err}`)
+        } finally {
+            dispatch({ type: "SET_LOADING", payload: false })
+        }
+    }
+}
+
+export function loadPostContent(title) {
+    return async (dispatch, getState) => {
+        try {
+            dispatch({ type: "SET_LOADING", payload: true })
+            const state = getState().userReducer
+            const response = await fetch(`http://localhost:6969/post/${title}`)
+            const { postData } = await response.json()
+
+            // Apply content to existing post info
+            const newPost = {
+                ...state.posts.find(post => post.title === title),
+                content: postData
+            }
+            console.log(newPost)
+            dispatch({ type: "FETCH_POSTDATA_SUCCESS", payload: newPost })
+        } catch (err) {
+            console.log(`Unable to load posts ${err}`)
         } finally {
             dispatch({ type: "SET_LOADING", payload: false })
         }
